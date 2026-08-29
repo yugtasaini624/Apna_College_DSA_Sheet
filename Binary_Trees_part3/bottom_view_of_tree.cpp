6de31e51f49f30d8b21fb5c61787eda192acc00d
Yugta Saini <yugtasaini@gmail.com>
2026-08-29 22:29:49 +0530

    Day 20

--- Binary_Trees_part3/bottom_view_of_tree.cpp
+++ Binary_Trees_part3/bottom_view_of_tree.cpp
@@ -1,6 +1,5 @@
 /*
 You are given the root of a binary tree, and your task is to return its bottom view. The bottom view of a binary tree is the set of nodes visible when the tree is viewed from the bottom.
-
 Note: If there are multiple bottom-most nodes for a horizontal distance from the root, then the latter one in the level order traversal is considered.
 
 Examples :
