0fb93eb1fd46effc2fbec2ec7a8a731099e81c9b
Yugta Saini <yugtasaini@gmail.com>
2026-09-05 20:14:35 +0530

    Day 27

--- Binary_Trees_part4/kth_ancestor.cpp
+++ Binary_Trees_part4/kth_ancestor.cpp
@@ -1,6 +1,5 @@
 /*
 You are given a tree with n nodes numbered from 0 to n - 1 in the form of a parent array parent where parent[i] is the parent of ith node. The root of the tree is node 0. Find the kth ancestor of a given node.
-
 The kth ancestor of a tree node is the kth node in the path from that node to the root node.
 
 Implement the TreeAncestor class:
