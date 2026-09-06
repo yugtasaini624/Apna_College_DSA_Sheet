66bd4dd49c03219f6baadc2fd8cf36fb315b7513
yugtasaini624 <yugtasaini@gmail.com>
2026-09-06 22:08:35 +0530

    Day 28

---
+++ BST_part1/kth_largest_in_bst.cpp
@@ -0,0 +1,101 @@
+/*
+You are part of a university admissions office and need to keep track of the kth highest test score from applicants in real-time. This helps to determine cut-off marks for interviews and admissions dynamically as new applicants submit their scores.
+
+You are tasked to implement a class which, for a given integer k, maintains a stream of test scores and continuously returns the kth highest test score after a new score has been submitted. More specifically, we are looking for the kth highest score in the sorted list of all scores.
+
+Implement the KthLargest class:
+
+    KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of test scores nums.
+    int add(int val) Adds a new test score val to the stream and returns the element representing the kth largest element in the pool of test scores so far.
+
+ 
+
+Example 1:
+
+Input:
+["KthLargest", "add", "add", "add", "add", "add"]
+[[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
+
+Output: [null, 4, 5, 5, 8, 8]
+
+Explanation:
+
+KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
+kthLargest.add(3); // return 4
+kthLargest.add(5); // return 5
+kthLargest.add(10); // return 5
+kthLargest.add(9); // return 8
+kthLargest.add(4); // return 8
+
+Example 2:
+
+Input:
+["KthLargest", "add", "add", "add", "add"]
+[[4, [7, 7, 7, 7, 8, 3]], [2], [10], [9], [9]]
+
+Output: [null, 7, 7, 7, 8]
+
+Explanation:
+KthLargest kthLargest = new KthLargest(4, [7, 7, 7, 7, 8, 3]);
+kthLargest.add(2); // return 7
+kthLargest.add(10); // return 7
+kthLargest.add(9); // return 7
+kthLargest.add(9); // return 8
+
+ 
+
+Constraints:
+
+    0 <= nums.length <= 104
+    1 <= k <= nums.length + 1
+    -104 <= nums[i] <= 104
+    -104 <= val <= 104
+    At most 104 calls will be made to add.
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class KthLargest {
+    int k;
+    priority_queue<int, vector<int>, greater<int>> minHeap;  
+public:
+    KthLargest(int k, vector<int>& nums) : k(k) {
+        for (int num : nums) {
+            if (minHeap.size() < k) {
+                minHeap.push(num);
+            } else if (num > minHeap.top()) {
+                minHeap.push(num);
+                if (minHeap.size() > k) {
+                    minHeap.pop();
+                }
+            }
+        }
+    }
+
+    int add(int val) {
+        if (minHeap.size() < k) {
+            minHeap.push(val);
+        } else if (val > minHeap.top()) {
+            minHeap.push(val);
+            minHeap.pop();
+        }
+        return minHeap.top();
+    }
+};
+
+int main() {
+    int k = 3;
+    vector<int> nums = {4, 5, 8, 2};
+
+    KthLargest kthLargest(k, nums);
+
+    cout << kthLargest.add(3) << endl;   // 4
+    cout << kthLargest.add(5) << endl;   // 5
+    cout << kthLargest.add(10) << endl;  // 5
+    cout << kthLargest.add(9) << endl;   // 8
+    cout << kthLargest.add(4) << endl;   // 8
+
+    return 0;
+}
