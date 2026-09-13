09e1db168750cd31fa26d4657cc2a6383162ed59
yugtasaini624 <yugtasaini@gmail.com>
2026-09-13 11:25:03 +0530

    Day 35

---
+++ BST_part2/construct_from_preorder.cpp
@@ -0,0 +1,87 @@
+/*
+Given an array of integers preorder, which represents the preorder traversal of a BST (i.e., binary search tree), construct the tree and return its root.
+
+It is guaranteed that there is always possible to find a binary search tree with the given requirements for the given test cases.
+
+A binary search tree is a binary tree where for every node, any descendant of Node.left has a value strictly less than Node.val, and any descendant of Node.right has a value strictly greater than Node.val.
+
+A preorder traversal of a binary tree displays the value of the node first, then traverses Node.left, then traverses Node.right.
+
+ 
+
+Example 1:
+
+Input: preorder = [8,5,1,7,10,12]
+Output: [8,5,10,1,7,null,12]
+
+Example 2:
+
+Input: preorder = [1,3]
+Output: [1,null,3]
+
+ 
+
+Constraints:
+
+    1 <= preorder.length <= 100
+    1 <= preorder[i] <= 1000
+    All the values of preorder are unique.
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int x) {
+        val = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+class Solution {
+public:
+    TreeNode* bstFromPreorder(vector<int>& A) {
+        int i = 0;
+        return build(A, i, INT_MAX);
+    }
+
+    TreeNode* build(vector<int>& A, int& i, int bound) {
+        if (i == A.size() || A[i] > bound)
+            return NULL;
+
+        TreeNode* root = new TreeNode(A[i++]);
+
+        root->left = build(A, i, root->val);
+        root->right = build(A, i, bound);
+
+        return root;
+    }
+};
+
+void inorder(TreeNode* root) {
+    if (root == NULL)
+        return;
+
+    inorder(root->left);
+    cout << root->val << " ";
+    inorder(root->right);
+}
+
+int main() {
+    vector<int> A = {8, 5, 1, 7, 10, 12};
+
+    Solution obj;
+
+    TreeNode* root = obj.bstFromPreorder(A);
+
+    cout << "Inorder traversal of BST: ";
+    inorder(root);
+
+    return 0;
+}
